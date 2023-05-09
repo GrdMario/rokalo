5cd2274ab43311909169eedc957a72ab901263d1
Antonio Ivancic <antonio.ivancic@rokolabs.com>
2023-05-09 13:52:43 +0200

    External dependencies, DependencyInjection class

---
+++ src/Application/Rokalo.Application/DependencyInjection.cs
@@ -0,0 +1,25 @@
+﻿namespace Rokalo.Application
+{
+    using FluentValidation;
+    using MediatR;
+    using Microsoft.Extensions.DependencyInjection;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplicationConfiguration(
+            this IServiceCollection services,
+            params Assembly[] assemblies)
+        {
+            services.AddMediatR(assemblies);
+
+            services.AddValidatorsFromAssemblies(assemblies, includeInternalTypes: true);
+            return services;
+        }
+    }
+}
